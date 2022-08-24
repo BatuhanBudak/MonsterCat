87265770fccbeba3a9c5bcf05817ba9e2139a4cf
Batuhan <batuhan.budak@gmail.com>
2022-08-24 23:01:21 +0300

    Refactor Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,44 +1,47 @@
-import { useState } from 'react'
-import useScreenSize from '../hooks/useScreenSize';
-import Nav from './Nav';
-import Image from 'next/image';
-import logo from '../public/icons/monstercat-logo.webp';
+import { useState } from "react";
+import useScreenSize from "../hooks/useScreenSize";
+import Nav from "./Nav";
+import Image from "next/image";
+import logo from "../public/icons/monstercat-logo.webp";
+import hamburger from "../public/icons/hamburger.svg";
 
 export default function Header() {
-    const [navOpen, setNavOpen] = useState(false);
-    const windowWidth = useScreenSize();
-    const desktopScreenWidth = 800;
+  const [navOpen, setNavOpen] = useState(false);
+  const windowWidth = useScreenSize();
+  const desktopScreenWidth = 800;
 
-    function toggleNav() {
-        if (windowWidth) {
-            if (windowWidth > desktopScreenWidth) return;
-            setNavOpen((prev) => !prev);
-        }
+  function toggleNav() {
+    if (windowWidth) {
+      if (windowWidth > desktopScreenWidth) return;
+      setNavOpen((prev) => !prev);
     }
+  }
 
-    function closeNav() {
-        if (windowWidth) {
-            if (windowWidth > desktopScreenWidth) return;
-            setNavOpen(false);
-        }
+  function closeNav() {
+    if (windowWidth) {
+      if (windowWidth > desktopScreenWidth) return;
+      setNavOpen(false);
     }
+  }
 
-    return (
-        <header className={`header ${navOpen ? 'nav-open' : ''}`}>
-
-
-            <a href="https://www.monstercat.com" className='logo-link'>
-                <Image src={logo} alt='monstercat logo' />
-            </a>
-
-            <button className="nav-toggle"
-                aria-expanded={navOpen}
-                onClick={toggleNav}
-                aria-controls="navigation">
-                <span className='hamburger'> </span>
-            </button>
-            <Nav closeNav={closeNav} />
-
-        </header>
-    )
+  return (
+    <header className={`header ${navOpen ? "nav-open" : ""}`}>
+      <div className="header-bar">
+        <div className="header-bar-content">
+          <a href="https://www.monstercat.com" className="logo-link">
+            <Image src={logo} alt="monstercat logo" />
+          </a>
+          <button
+            className="nav-toggle"
+            aria-expanded={navOpen}
+            onClick={toggleNav}
+            aria-controls="navigation"
+          >
+            <Image src={hamburger} alt="navigation" />
+          </button>
+        </div>
+      </div>
+      <Nav closeNav={closeNav} />
+    </header>
+  );
 }
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -3,225 +3,288 @@ import useScreenSize from "../hooks/useScreenSize";
 import Image from "next/image";
 import logoLarge from "../public/icons/monstercat-logo-nav.svg";
 import useToggle from "../hooks/useToggle";
+import close from "../public/icons/close.svg";
+import twitter from "../public/icons/twitter.svg";
+import instagram from "../public/icons/instagram.svg";
+import twitch from "../public/icons/twitch.svg";
+import tiktok from "../public/icons/tiktok.svg";
+import facebook from "../public/icons/facebook.svg";
+import player from "../public/icons/player-small.svg";
 
-export default function Nav({ }) {
-    const windowWidth = useScreenSize();
-    const [toggledFirst, toggleStateFirst] = useToggle();
-    const [toggledSecond, toggleStateSecond] = useToggle();
-    const [toggledThird, toggleStateThird] = useToggle();
-    const [toggledFourth, toggleStateFourth] = useToggle();
+export default function Nav({ closeNav }) {
+  const windowWidth = useScreenSize();
+  const [toggledFirst, toggleStateFirst] = useToggle();
+  const [toggledSecond, toggleStateSecond] = useToggle();
+  const [toggledThird, toggleStateThird] = useToggle();
+  const [toggledFourth, toggleStateFourth] = useToggle();
 
-    if (windowWidth < 800) {
-        return (
-            <nav>
-                <div className="nav-logo">
-                    <Image src={logoLarge} alt="" />
-                </div>
-                <ul className="navigation" id="navigation">
-                    <li >
-                        <a onClick={toggleStateFirst} className="nav-link nav__item has-content"
-                        >
-                            MUSIC
-                        </a>
-                        {toggledFirst && <ul className="nav__item-sublist">
-                            <li>
-                                <a
-                                    className="nav__item-sublist-link"
-                                    href="https://www.monstercat.com/music"
-                                >
-                                    OUR MUSIC
-                                </a>
-                            </li>
-                            <li>
-                                <a className="nav__item-sublist-link" href="">
-                                    INSTINCT
-                                </a>
-                            </li>
-                            <li>
-                                <a className="nav__item-sublist-link" href="">
-                                    UNCAGED
-                                </a>
-                            </li>
-                            <li>
-                                <a className="nav__item-sublist-link" href="">
-                                    SILK
-                                </a>
-                            </li>
-                        </ul>}
-                    </li>
-                    <li className="nav__item">
-                        <a href="" className="nav-link">
-                            ARTISTS
-                        </a>
+  if (windowWidth < 800) {
+    return (
+      <nav id="navigation" className="navigation">
+        <div className="navigation-container">
+          <div className="nav-top">
+            <a className="nav-logo">
+              <Image src={logoLarge} alt="" />
+            </a>
+            <div className="close-button" onClick={closeNav}>
+              <Image src={close} alt="" />
+            </div>
+          </div>
+          <div className="navigation-list-container">
+            <ul className="navigation-list">
+              <li className="nav__item">
+                <a
+                  onClick={toggleStateFirst}
+                  className="nav-link nav__item has-content"
+                >
+                  MUSIC
+                </a>
+                {toggledFirst && (
+                  <ul className="nav__item-sublist">
+                    <li>
+                      <a
+                        className="nav__item-sublist-link"
+                        href="https://www.monstercat.com/music"
+                      >
+                        OUR MUSIC
+                      </a>
                     </li>
                     <li>
-                        <a
-
-                            className="nav-link nav__item has-content"
-                            onClick={toggleStateSecond}
-                        >
-                            ABOUT
-                        </a>
-                        {toggledSecond && <ul className="nav__item-sublist">
-                            <li>
-                                <a className="nav__item-sublist-link" href="">
-                                    About Monstercat
-                                </a>
-                            </li>
-                            <li>
-                                <a className="nav__item-sublist-link" href="">
-                                    Diversity {"&"} Inclusion
-                                </a>
-                            </li>
-                            <li>
-                                <a className="nav__item-sublist-link" href="">
-                                    Code of Ethics
-                                </a>
-                            </li>
-                            <li>
-                                <a className="nav__item-sublist-link" href="">
-                                    Environmental
-                                </a>
-                            </li>
-                            <li>
-                                <a className="nav__item-sublist-link" href="">
-                                    Contact Us
-                                </a>
-                            </li>
-                        </ul>}
+                      <a className="nav__item-sublist-link" href="">
+                        INSTINCT
+                      </a>
                     </li>
-                    <li className="nav__item">
-                        <a
-                            href=""
-                            className="nav-link"
-                        >
-                            NEWS
-                        </a>
+                    <li>
+                      <a className="nav__item-sublist-link" href="">
+                        UNCAGED
+                      </a>
                     </li>
-                    <li className="nav__item">
-                        <a
-
-                            className="nav-link nav__item has-content"
-                            onClick={toggleStateThird}
-                        >
-                            EVENTS
-                        </a>
-                        {toggledThird && <ul className="nav__item-sublist">
-                            <li>
-                                <a className="nav__item-sublist-link" href="">
-                                    Monstercat Events Experience
-                                </a>
-                            </li>
-                            <li>
-                                <a className="nav__item-sublist-link" href="">
-                                    Upcoming Events
-                                </a>
-                            </li>
-                        </ul>}
+                    <li>
+                      <a className="nav__item-sublist-link" href="">
+                        SILK
+                      </a>
                     </li>
-                    <li >
-                        <a
-                            className="nav-link nav__item has-content"
-                            onClick={toggleStateFourth}
-                        >
-                            PROGRAMMING
-                        </a>
-                        {toggledFourth && <ul className="nav__item-sublist">
-                            <li>
-                                <a className="nav__item-sublist-link" href="">
-                                    MonstercatTV
-                                </a>
-                            </li>
-                            <li>
-                                <a className="nav__item-sublist-link" href="">
-                                    Call of the Wild
-                                </a>
-                            </li>
-                            <li>
-                                <a className="nav__item-sublist-link" href="">
-                                    Silk Showcase
-                                </a>
-                            </li>
-                            <li>
-                                <a className="nav__item-sublist-link" href="">
-                                    Upcoming Shows
-                                </a>
-                            </li>
-                        </ul>}
+                  </ul>
+                )}
+              </li>
+              <li className="nav__item">
+                <a href="" className="nav-link">
+                  ARTISTS
+                </a>
+              </li>
+              <li className="nav__item">
+                <a
+                  className="nav-link nav__item has-content"
+                  onClick={toggleStateSecond}
+                >
+                  ABOUT
+                </a>
+                {toggledSecond && (
+                  <ul className="nav__item-sublist">
+                    <li>
+                      <a className="nav__item-sublist-link" href="">
+                        About Monstercat
+                      </a>
                     </li>
-                    <li className="nav__item">
-                        <a href="" className="nav-link">
-                            GOLD
-                        </a>
+                    <li>
+                      <a className="nav__item-sublist-link" href="">
+                        Diversity {"&"} Inclusion
+                      </a>
                     </li>
-                    <li className="nav__item">
-                        <a href="" className="nav-link">
-                            PARTNERS
-                        </a>
+                    <li>
+                      <a className="nav__item-sublist-link" href="">
+                        Code of Ethics
+                      </a>
                     </li>
-                    <li className="nav__item">
-                        <a href="" className="nav-link">
-                            PRESS
-                        </a>
+                    <li>
+                      <a className="nav__item-sublist-link" href="">
+                        Environmental
+                      </a>
                     </li>
-                    <li className="nav__item">
-                        <a href="" className="nav-link">
-                            PLAYER
-                        </a>
+                    <li>
+                      <a className="nav__item-sublist-link" href="">
+                        Contact Us
+                      </a>
                     </li>
-                    <li className="nav__item">
-                        <a href="/contact" className="nav-link">
-                            SHOP
-                        </a>
+                  </ul>
+                )}
+              </li>
+              <li className="nav__item">
+                <a href="" className="nav-link">
+                  NEWS
+                </a>
+              </li>
+              <li className="nav__item">
+                <a
+                  className="nav-link nav__item has-content"
+                  onClick={toggleStateThird}
+                >
+                  EVENTS
+                </a>
+                {toggledThird && (
+                  <ul className="nav__item-sublist">
+                    <li>
+                      <a className="nav__item-sublist-link" href="">
+                        Monstercat Events Experience
+                      </a>
                     </li>
-                    <li className="nav__item">
-                        <a href="" className="nav-link">
-                            LOST CIVILIZATION
-                        </a>
+                    <li>
+                      <a className="nav__item-sublist-link" href="">
+                        Upcoming Events
+                      </a>
                     </li>
-                </ul>
-            </nav>
-        );
-    } else {
-        return (
-            <nav>
-                <ul className="navigation" id="navigation">
-                    <li className="nav__item">
-                        <a
-                            href="https://www.instrument.com/what-we-do"
-                            className="nav-link"
-                        >
-                            WHAT WE DO
-                        </a>{" "}
+                  </ul>
+                )}
+              </li>
+              <li>
+                <a
+                  className="nav-link nav__item has-content"
+                  onClick={toggleStateFourth}
+                >
+                  PROGRAMMING
+                </a>
+                {toggledFourth && (
+                  <ul className="nav__item-sublist">
+                    <li>
+                      <a className="nav__item-sublist-link" href="">
+                        MonstercatTV
+                      </a>
                     </li>
-                    <li className="nav__item">
-                        <a href="https://www.instrument.com/work className='nav-link'">
-                            WORK
-                        </a>{" "}
+                    <li>
+                      <a className="nav__item-sublist-link" href="">
+                        Call of the Wild
+                      </a>
                     </li>
-                    <li className="nav__item">
-                        <a
-                            href="https://www.instrument.com/who-we-are"
-                            className="nav-link"
-                        >
-                            WHO WE ARE
-                        </a>{" "}
+                    <li>
+                      <a className="nav__item-sublist-link" href="">
+                        Silk Showcase
+                      </a>
                     </li>
-                    <li className="nav__item">
-                        <a
-                            href="https://www.instrument.com/being-here"
-                            className="nav-link"
-                        >
-                            BEING HERE
-                        </a>{" "}
+                    <li>
+                      <a className="nav__item-sublist-link" href="">
+                        Upcoming Shows
+                      </a>
                     </li>
-                    <li className="nav__item">
-                        <a href="https://www.instrument.com/careers" className="nav-link">
-                            CAREERS
-                        </a>{" "}
-                    </li>
-                </ul>
-            </nav>
-        );
-    }
+                  </ul>
+                )}
+              </li>
+              <li className="nav__item">
+                <a href="" className="nav-link">
+                  GOLD
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="" className="nav-link">
+                  PARTNERS
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="" className="nav-link">
+                  PRESS
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="" className="nav-link">
+                  PLAYER
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="/contact" className="nav-link">
+                  SHOP
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="" className="nav-link">
+                  LOST CIVILIZATION
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="navigation-bottom">
+            <ul className="nav-social-links-list">
+              <li>
+                <a href="">
+                  <Image src={instagram} alt="" />
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  <Image src={twitter} alt="" />
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  <Image src={tiktok} alt="" />
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  <Image src={twitch} alt="" />
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  <Image src={facebook} alt="" />
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  <Image src={player} alt="" />
+                </a>
+              </li>
+            </ul>
+            <div className="nav-bottom-links-container">
+              <a href="" className="nav-bottom-signin">
+                SIGN IN
+              </a>
+              <a href="" className="nav-bottom-signup">
+                OR SIGN UP
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    );
+  } else {
+    return (
+      <nav>
+        <ul className="navigation" id="navigation">
+          <li className="nav__item">
+            <a
+              href="https://www.instrument.com/what-we-do"
+              className="nav-link"
+            >
+              WHAT WE DO
+            </a>{" "}
+          </li>
+          <li className="nav__item">
+            <a href="https://www.instrument.com/work className='nav-link'">
+              WORK
+            </a>{" "}
+          </li>
+          <li className="nav__item">
+            <a
+              href="https://www.instrument.com/who-we-are"
+              className="nav-link"
+            >
+              WHO WE ARE
+            </a>{" "}
+          </li>
+          <li className="nav__item">
+            <a
+              href="https://www.instrument.com/being-here"
+              className="nav-link"
+            >
+              BEING HERE
+            </a>{" "}
+          </li>
+          <li className="nav__item">
+            <a href="https://www.instrument.com/careers" className="nav-link">
+              CAREERS
+            </a>{" "}
+          </li>
+        </ul>
+      </nav>
+    );
+  }
 }
